365a29904c874cd7acf3347f1ff5263def6164f6
Valerio <valerio.colantonio@sngular.com>
2021-10-25 14:01:39 +0200

    Added new views

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Valerio Colantonio on 25/10/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
